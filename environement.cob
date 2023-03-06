19bbbd853e2ac2c36f1a622a3c202d783aeb90bc
Cédric Merlet <cedric.merlet@hotmail.com>
2023-03-06 18:23:04 +0100

    initialisation de l'environnement pour le logiciel de gestion des serres

--- environement.cob
+++ environement.cob
@@ -1,51 +1,51 @@
         IDENTIFICATION DIVISION.
         PROGRAM-ID. projet_serres.
-        
+
         ENVIRONMENT DIVISION.
         INPUT-OUTPUT SECTION.
         FILE-CONTROL.
-        
+
                 SELECT FPlantes ASSIGN TO "plantes.dat"
-                ORGANIZATION indexed 
+                ORGANIZATION indexed
                 ACCESS MODE IS dynamic
                 RECORD KEY IS fp_nomLatin
                 ALTERNATE RECORD KEY IS fp_nomCommun
                 ALTERNATE RECORD KEY IS fp_nomVulgaire WITH DUPLICATES
                 FILE STATUS IS cr_fPlantes.
-                
+
                 SELECT FUtilisateurs ASSIGN TO "utilisateurs.dat"
-                ORGANIZATION indexed 
+                ORGANIZATION indexed
                 ACCESS MODE IS dynamic
                 RECORD KEY IS fu_idUtilisateur
                 ALTERNATE RECORD KEY IS fu_mail
                 FILE STATUS IS cr_fUtilisateurs.
-                
+
                 SELECT FSerres ASSIGN TO "serres.dat"
-                ORGANIZATION indexed 
+                ORGANIZATION indexed
                 ACCESS MODE IS dynamic
                 RECORD KEY IS fs_nomSerre
                 ALTERNATE RECORD KEY IS fs_idUtilisateur WITH DUPLICATES
                 FILE STATUS IS cr_fSerres.
-                
+
                 SELECT FZones ASSIGN TO "zones.dat"
-                ORGANIZATION indexed 
+                ORGANIZATION indexed
                 ACCESS MODE IS dynamic
                 RECORD KEY IS fz_cle
                 ALTERNATE RECORD KEY IS fz_numZone
                 ALTERNATE RECORD KEY IS fz_nomSerre WITH DUPLICATES
                 FILE STATUS IS cr_fZones.
-                
+
                 SELECT FCompatibilites ASSIGN TO "compatibilite.dat"
-                ORGANIZATION indexed 
+                ORGANIZATION indexed
                 ACCESS MODE IS dynamic
                 RECORD KEY IS fc_cle
                 ALTERNATE RECORD KEY IS fc_nomLatin_1 WITH DUPLICATES
                 ALTERNATE RECORD KEY IS fc_nomLatin_2 WITH DUPLICATES
-                FILE STATUS IS cr_fCompatibilites.  
-                
+                FILE STATUS IS cr_fCompatibilites.
+
         DATA DIVISION.
-        FILE SECTION.           
-                
+        FILE SECTION.
+
         FD FPlantes.
         01 tamp_fPlantes.
                 02 fp_nomLatin PIC A(50).
@@ -57,7 +57,7 @@
                 02 fp_soleil PIC A(20).
                 02 fp_sol PIC A(20).
                 02 fp_description PIC A(255).
-                
+
         FD FUtilisateurs.
         01 tamp_fUtilisateurs.
                 02 fu_idUtilisateur PIC 9(3).
@@ -65,7 +65,7 @@
                 02 fu_prenom PIC A(30).
                 02 fu_mail PIC A(50).
                 02 fu_type PIC A(10).
-                
+
         FD FSerres.
         01 tamp_fSerres.
                 02 fs_nomSerre PIC 9(3).
@@ -75,13 +75,13 @@
 
         FD FZones.
         01 tamp_fZones.
-                02 fz_cle. 
+                02 fz_cle.
                         03 fz_numZone PIC 9(3).
                         03 fz_nomSerre PIC 9(3).
                 02 fz_nomLatin PIC A(50).
                 02 fz_exposition PIC A(20).
                 02 fz_sol PIC A(20).
-                
+
         FD FCompatibilites.
         01 tamp_fCompatibilite.
                 02 fc_cle.
@@ -107,39 +107,36 @@
         77 soleil PIC A(20).
         77 sol PIC A(20).
         77 description PIC A(255).
-        
+
         PROCEDURE DIVISION.
-        
+
         OPEN I-O FPlantes
                 IF cr_fPlantes=35 THEN
                 OPEN OUTPUT FPlantes
                 END-IF
         CLOSE FPlantes
-        
+
         OPEN I-O FUtilisateurs
                 IF cr_fUtilisateurs=35 THEN
                 OPEN OUTPUT FUtilisateurs
                 END-IF
         CLOSE FUtilisateurs
-        
+
         OPEN I-O FSerres
                 IF cr_fSerres=35 THEN
                 OPEN OUTPUT FSerres
                 END-IF
         CLOSE FSerres
-        
+
         OPEN I-O FZones
                 IF cr_fZones=35 THEN
                 OPEN OUTPUT FZones
                 END-IF
         CLOSE FZones
-                
+
         OPEN I-O FCompatibilites
                 IF cr_fCompatibilites=35 THEN
                 OPEN OUTPUT FCompatibilites
                 END-IF
-        CLOSE FCompatibilites 
-        PERFORM afficher_plantes
-        PERFORM ajouter_plante
+        CLOSE FCompatibilites
         STOP RUN.
-        COPY "plantes.cpy".
